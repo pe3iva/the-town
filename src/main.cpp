207574b52f20a48355125e44aa77ffac6956e1f0
pe3iva <dawgsofast@protonmail.com>
2025-02-22 23:20:35 +0100

    add vars and some basic intro

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
+// main place for navigating to other places and basic introduction
+
+// information about the player stored in variables is written with user_whateverInfoAboutThePlayer
+
 int main(){
 
-    
+    // Basic information
+    std::string user_name;
+    int user_age;
+    int user_day, user_month, user_year;
+
+    // Introduction
+    std::cout << "Welcome to The Town!\n";
+
+    do{
+        std::cout << "What is your name?: ";
+        std::cin >> user_name;
+    }while(user_name.empty());
 
 }
