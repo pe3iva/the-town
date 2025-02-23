59895c0788de26800ab1d21e92e9d2e3a096f5b5
pe3iva <dawgsofast@protonmail.com>
2025-02-23 09:50:46 +0100

    add basic questions along with user input
    removed the user_age var and added a do while loop for the age

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@ int main(){
 
     // Basic information
     std::string user_name;
-    int user_age;
     int user_day, user_month, user_year;
 
     // Introduction
@@ -19,4 +18,10 @@ int main(){
         std::cin >> user_name;
     }while(user_name.empty());
 
+    do{
+        std::cout << "What is your date of birth?: ";
+        std::cin >> user_month;
+        std::cin >> user_day;
+        std::cin >> user_year;
+    }while(user_day < 0 && user_day > 31 && user_month < 0 && user_month > 12 && user_year <= 1902 && user_year > 2025);
 }
