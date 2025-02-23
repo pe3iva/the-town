f08795adb026c7480a35cf4acf89075e7af33d64
pe3iva <dawgsofast@protonmail.com>
2025-02-23 15:43:29 +0100

    add selection menu & improve readability

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,10 @@
 int main(){
 
     // Basic information
+    // we store user info so later on we can identify who is who
     std::string user_name;
     int user_day, user_month, user_year;
+    // Basic information
 
     // Introduction
     std::cout << "Welcome to The Town!\n";
@@ -23,7 +25,36 @@ int main(){
         std::cin >> user_month;
         std::cin >> user_day;
         std::cin >> user_year;
-    }while(user_day < 0 && user_day > 31 && user_month < 0 && user_month > 12 && user_year <= 1902 && user_year > 2025);
+    }while(user_day <= 0 && user_day > 31 && user_month <= 0 && user_month > 12 && user_year <= 1902 && user_year > 2025);
 
-    std::cout << "Welcome " << user_name << "!" << '\n';
+    std::cout << "Welcome " << user_name << "! " << "You're ready to start playing.\n";
+    // Introduction
+
+    // Navigation menu
+
+    // Information
+    bool hasHome = false;
+    double user_money = 0;
+    int selection;
+
+    std::cout << "1." << " Go to your house" << '\n';
+    std::cout << "2." << " Shop" << '\n';
+    std::cout << "3." << " Jobs" << '\n';
+    std::cout << "4." << " Help" << '\n';
+
+    if(selection == 1){
+        std::cout << "1" << '\n';
+    }
+    else if(selection == 2){
+        std::cout << "2" << '\n';
+    }
+    else if(selection == 3){
+        std::cout << "3" << '\n';
+    }
+    else if(selection == 4){
+        std::cout << "4" << '\n';
+    }
+    else{
+        std::cout << "Invalid selection (1-4)\n";
+    }
 }
